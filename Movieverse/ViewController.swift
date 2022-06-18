28ef08fe5d0437f889fa6405c30c649c764607c0
Mert Gökduman <mert.gokduman@external.yapikredi.com.tr>
2022-06-18 15:04:11 +0300

    Initial Commit

---
+++ Movieverse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movieverse
+//
+//  Created by Mert Gökduman on 18.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
